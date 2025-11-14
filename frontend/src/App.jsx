e3390496d3b064366f26d226e2d0fcc918728e02
connect-aima <aimaabbas301@gmail.com>
2025-11-14 19:35:23 +0500

    Added assets and components folder in src and added navbar in components folder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='app'>
+     <Navbar/>
     </div>
   )
 }
