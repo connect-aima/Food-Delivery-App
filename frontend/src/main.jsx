fdc67a7fe4c9c222db1b5c5e39b5dfde7e231a6c
connect-aima <aimaabbas301@gmail.com>
2025-11-16 00:22:19 +0500

    added Contextapi to my app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import StoreContextProvider from './Context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+
   </BrowserRouter>
 )
-  
+
