e755a1d5e8f5ad185241e01742f16565c4d7a6cf
connect-aima <aimaabbas301@gmail.com>
2025-11-18 00:07:35 +0500

    added add to cart and remove to cart functionality by making changes in storeContext,foodItems and foodDisplay and remove a minor but serious bug

--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,7 @@ const FoodDisplay = (category) => {
             <h2>Top Dishes near you</h2>
             <div className="food-display-list">
                 {food_list.map((item,index)=>{
-                    return <FoodItems key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                    return <FoodItems key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
                 })}
             </div>
         </div>
--- frontend/src/Components/FoodItems/FoodItems.jsx
+++ frontend/src/Components/FoodItems/FoodItems.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import './FoodItems.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../Context/StoreContext'
 const FoodItems = ({ id, name, price, description, image }) => {
-    const [itemCount, setItemCount] = useState(0)
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
   return (
     <div className='food-item'>
         <div className='food-item-imge-container'>
             <img className='food-item-image' src={image} alt="" />
+        {!cartItems[id]
+            ?<img className='add' onClick={()=>addToCart(id) } src={assets.add_icon_white} /> 
+            :<div className='food-item-counter'>
+                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red}/>
+                <p>{cartItems[id]}</p>
+                <img onClick={()=>addToCart(id)} src={assets.add_icon_green}/>
+            </div>
+
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-team-rating">
