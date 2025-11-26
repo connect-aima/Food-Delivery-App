a80dc5b724a56feb177209f3df070fc9b456db36
connect-aima <aimaabbas301@gmail.com>
2025-11-26 16:40:18 +0500

    added functionality to get total cart amount

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { use, useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../Context/StoreContext'
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart ,getTotalCartAmount } = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -44,17 +44,17 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>SubTotal</p>
-              <p>{0}</p>
+              <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>${5}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{0}</b>
+              <b>${getTotalCartAmount()+5}</b>
             </div>
           </div>
             <button>PROCEED TO CHECK OUT</button>
