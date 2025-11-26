2233198d44faada880342442274b7394534a0845
connect-aima <aimaabbas301@gmail.com>
2025-11-26 16:25:10 +0500

    added cart-page

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-const Navbar = ({setShowLogin}) => {
+import { Link } from 'react-router-dom'
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home")
   return (
     <div className='navbar'>
+      <Link to='./'>
       <img src={assets.logo} alt="" className='logo' />
+      </Link>
       <ul className="navbar-menu">
-        <li onClick={()=>{
-          setMenu("home")
-        }} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>{
+        <Link to='./'>
+          <li onClick={() => {
+            setMenu("home")
+          }} className={menu === "home" ? "active" : ""}>Home</li>
+        </Link>
+        <Link>
+        <li onClick={() => {
           setMenu("menu")
-        }} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=>{
+        }} className={menu === "menu" ? "active" : ""}>Menu</li>
+        </Link>
+        <Link>
+        <li onClick={() => {
           setMenu("mobile-app")
-        }} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
-        <li onClick={()=>{
+        }} className={menu === "mobile-app" ? "active" : ""}>Mobile-App</li>
+        </Link>
+        <Link>
+        <li onClick={() => {
           setMenu("contact-us")
-        }} className={menu==="contact-us"?"active":""}>Contact Us</li>
+        }} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
+        </Link>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+          <Link to='./cart'><img src={assets.basket_icon} alt="" /></Link>
+          <div className="dot"></div>
         </div>
-        <button onClick={()=>{setShowLogin(true)}}>Sign in</button>
+        <button onClick={() => { setShowLogin(true) }}>Sign in</button>
       </div>
     </div>
   )
